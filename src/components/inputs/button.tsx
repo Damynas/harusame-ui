e449d56fe93ccf15911e35eefcc1f093c4b1ce69
damynas_povilas <povilas.damynas@devbridge.com>
2024-01-26 15:05:51 +0200

    Updated button component to allow ref forwarding

--- src/components/inputs/button.tsx
+++ src/components/inputs/button.tsx
@@ -1,6 +1,10 @@
-import { type ButtonHTMLAttributes, type FC } from 'react';
-import resources from './button.resources.json';
+import {
+  forwardRef,
+  type ButtonHTMLAttributes,
+  type ForwardedRef
+} from 'react';
 import styled from 'styled-components';
+import resources from './button.resources.json';
 
 type ButtonProps = {
   label: string;
@@ -15,20 +19,47 @@ const OutlinedButton = styled(ButtonBase)``;
 
 const TextButton = styled(ButtonBase)``;
 
-const Button: FC<ButtonProps> = (props) => {
-  const { label, variant = resources.defaultButtonVariant, ...rest } = props;
-
+const Button = (
+  buttonProps: ButtonProps,
+  forwardedRef: ForwardedRef<HTMLButtonElement>
+) => {
+  const {
+    label,
+    variant = resources.defaultButtonVariant,
+    ...props
+  } = buttonProps;
   switch (variant) {
     case 'contained':
-      return <ContainedButton {...rest}>{label}</ContainedButton>;
+      return (
+        <ContainedButton
+          {...props}
+          ref={forwardedRef}
+        >
+          {label}
+        </ContainedButton>
+      );
     case 'outlined':
-      return <OutlinedButton {...rest}>{label}</OutlinedButton>;
+      return (
+        <OutlinedButton
+          {...props}
+          ref={forwardedRef}
+        >
+          {label}
+        </OutlinedButton>
+      );
     case 'text':
-      return <TextButton {...rest}>{label}</TextButton>;
+      return (
+        <TextButton
+          {...props}
+          ref={forwardedRef}
+        >
+          {label}
+        </TextButton>
+      );
     default:
       throw new Error(`Unsupported button variant: ${variant}`);
   }
 };
 
-export default Button;
+export default forwardRef<HTMLButtonElement, ButtonProps>(Button);
 export type { ButtonProps };
