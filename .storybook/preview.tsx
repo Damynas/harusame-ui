eb7bcb1fce3e46e8fb6616ee0a13612f7f1f8d57
Damynas <povdam6@gmail.com>
2024-02-22 21:39:23 +0200

    Storybook configured to work with theme and font providers

---
+++ .storybook/preview.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import type { Decorator, Parameters, Preview } from '@storybook/react';
+import { Themes, ThemeProvider } from '../src/common/theme';
+import { Font } from '../src/common/font';
+import { convertCase } from '../src/utils';
+
+const themeNames = Object.getOwnPropertyNames(Themes).map((theme) =>
+  convertCase(theme, 'camelCase', 'titleCase')
+);
+
+const parameters: Parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/i
+    }
+  }
+};
+
+const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: themeNames.length && themeNames.at(0),
+    toolbar: {
+      icon: 'lightning',
+      items: themeNames,
+      dynamicTitle: true
+    }
+  }
+};
+
+const withThemeProvider: Decorator = (Story, context) => {
+  const themeName = context.globals.theme;
+  const theme = {
+    ...Themes[convertCase(themeName, 'titleCase', 'camelCase')]
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <Font />
+      <Story />
+    </ThemeProvider>
+  );
+};
+
+const decorators = [withThemeProvider];
+
+const preview: Preview = {
+  parameters,
+  globalTypes,
+  decorators
+};
+
+export default preview;
